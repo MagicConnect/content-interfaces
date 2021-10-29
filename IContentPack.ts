f0180d1f780e34e299441fed89ddf79d747b4cd0
Kyle Kemp <kyle@seiyria.com>
2021-10-29 13:15:17 -0500

    add enemy interface

--- IContentPack.ts
+++ IContentPack.ts
@@ -5,11 +5,13 @@ import { IChip } from './IChip';
 import { IItem } from './IItem';
 import { IWeapon } from './IWeapon';
 import { IShop } from './IShop';
+import { IEnemy } from './IEnemy';
 
 export interface IContentPack {
   banners: IBanner[];
   characters: ICharacter[];
   chips: IChip[];
+  enemies: IEnemy[];
   items: IItem[];
   shops: IShop[];
   weapons: IWeapon[];

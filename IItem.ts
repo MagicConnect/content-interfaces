6fa34db103bcb821f4f3032098db5a11e3abceff
Kyle Kemp <kyle@seiyria.com>
2021-11-02 09:54:14 -0500

    closes #9

--- IItem.ts
+++ IItem.ts
@@ -1,6 +1,25 @@
 
+export enum ItemType {
+  None = 'None',
+  
+  Weapon = 'Weapon',
+  Chip = 'Chip',
+  ShopToken = 'ShopToken',
+  WeaponThreeStarDraw = 'WeaponThreeStarDraw',
+  WeaponFourStarDraw = 'WeaponFourStarDraw',
+  WeaponFiveStarDraw = 'WeaponFiveStarDraw',
+  ChipThreeStarDraw = 'ChipThreeStarDraw',
+  ChipFourStarDraw = 'ChipFourStarDraw',
+  ChipFiveStarDraw = 'ChipFiveStarDraw',
+  CharacterThreeStarDraw = 'CharacterThreeStarDraw',
+  CharacterFourStarDraw = 'CharacterFourStarDraw',
+  CharacterFiveStarDraw = 'CharacterFiveStarDraw',
+  Sticker = 'Sticker'
+}
+
 export interface IItem {
   name: string;
   description: string;
   sellValue: number;
+  itemType: ItemType;
 }

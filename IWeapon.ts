70fa0121754bd20bc2f3ee009f8f19601fb113a4
Kyle Kemp <kyle@seiyria.com>
2021-12-07 12:04:52 -0600

    closes #55

--- IWeapon.ts
+++ IWeapon.ts
@@ -1,7 +1,9 @@
+import { Weapon } from './BuildingBlocks';
 import { PrimaryStat } from './BuildingBlocks';
 import { IItem } from './IItem';
 
-export type IWeapon = IItem & {      
+export type IWeapon = IItem & {
+  weaponType: Weapon;                     // the type of weapon
   stars: 1|2|3|4|5;                       // weapon rarity   
   primaryStat: PrimaryStat;               // weapon main stat
   secondaryStat?: PrimaryStat;            // weapon secondary stat

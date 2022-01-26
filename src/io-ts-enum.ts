8b8c03be26dc66414d9363420d5e25de917e2414
Raymond Hammarling <raymond@hammarlings.se>
2022-01-26 15:41:11 +0100

    Fix type incompatibility with number enums and enumT

--- src/io-ts-enum.ts
+++ src/io-ts-enum.ts
@@ -1,6 +1,6 @@
 import * as t from 'io-ts';
 
-export function enumT<V extends string | number>(name: string, e: Record<string, V>): t.Type<V, V, unknown> {
+export function enumT<V extends string | number>(name: string, e: Record<string, string | V>): t.Type<V, V, unknown> {
   const isEnumValue = (input: unknown): input is V => Object.values<unknown>(e).includes(input);
 
   return new t.Type<V, V, unknown>(

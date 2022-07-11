977a8f33534a7ef8142ac528be779fca8077d3b1
Jake Hein <jacobnolanhein@gmail.com>
2022-07-11 14:15:23 -0700

    final change

--- src/IStore.ts
+++ src/IStore.ts
@@ -3,34 +3,28 @@ import { ItemType } from "./IItem";
 import * as t from 'io-ts';
 import { enumT } from "./io-ts-enum";
 
-export const itemTypeT = enumT('ItemType', ItemType);
+export const storeItemTypeT = enumT('ItemType', ItemType);
 
-export interface IItemBundle {
+export interface IStoreItemBundle {
   itemType: ItemType;
   quantity: number;
 }
 
-export const itemBundleT: t.Type<IItemBundle> = t.type({
-  itemType: itemTypeT,
+export const storeItemBundleT: t.Type<IStoreItemBundle> = t.type({
+  itemType: storeItemTypeT,
   quantity: t.number,
 })
 
 export interface IStore extends IIdentifiable {
   id: string;
   name: string;
-  items: IItemBundle[];
+  items: IStoreItemBundle[];
   cost: number;
 }
 
 export const storeT: t.Type<IStore> = t.type({
   id: t.string,
   name: t.string,
-  items: t.array(itemBundleT),
+  items: t.array(storeItemBundleT),
   cost: t.number,
 });
-
-// export interface IBundle {
-//   bundle: Record<string, IStore>;
-// }
-
-// export declare const bundleT: t.Type<IBundle>;

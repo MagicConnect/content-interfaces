d722179455314274aad469146fca3e2d46242cd9
Kyle Kemp <kyle@seiyria.com>
2022-07-20 11:49:59 -0500

    update matrices

--- src/Matrices.ts
+++ src/Matrices.ts
@@ -15,9 +15,9 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 6,
       [Stat.Defense]: 2,
       [Stat.Magic]: 0,
-      [Stat.Special]: 0,
-  
       [Stat.HP]: 40,
+  
+      [Stat.Special]: 0,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
@@ -27,24 +27,24 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 4,
       [Stat.Defense]: 0,
       [Stat.Magic]: 0,
-      [Stat.Special]: 1,
-  
       [Stat.HP]: 6,
+
+      [Stat.Special]: 1,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
     },
 
     limitBreakPoints: {
-      [Stat.Attack]: 4,
+      [Stat.Attack]: 0,
       [Stat.Defense]: 0,
       [Stat.Magic]: 0,
-      [Stat.Special]: 2,
-  
-      [Stat.HP]: 20,
-      [Stat.Accuracy]: 0,
-      [Stat.Critical]: 0,
-      [Stat.Evasion]: 0,
+      [Stat.HP]: 0,
+
+      [Stat.Special]: 1,
+      [Stat.Accuracy]: 0.2,
+      [Stat.Critical]: 0.6,
+      [Stat.Evasion]: 0.2,
     }
   },
 
@@ -53,9 +53,9 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 1,
       [Stat.Defense]: 0,
       [Stat.Magic]: 5,
-      [Stat.Special]: 2,
-  
       [Stat.HP]: 10,
+
+      [Stat.Special]: 2,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
@@ -65,24 +65,24 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 0,
       [Stat.Defense]: 0,
       [Stat.Magic]: 4,
-      [Stat.Special]: 2,
-  
       [Stat.HP]: 2,
+
+      [Stat.Special]: 2,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
     },
 
     limitBreakPoints: {
-      [Stat.Attack]: 2,
+      [Stat.Attack]: 0,
       [Stat.Defense]: 0,
-      [Stat.Magic]: 4,
-      [Stat.Special]: 2,
-  
-      [Stat.HP]: 10,
-      [Stat.Accuracy]: 0,
-      [Stat.Critical]: 0,
-      [Stat.Evasion]: 0,
+      [Stat.Magic]: 0,
+      [Stat.HP]: 0,
+
+      [Stat.Special]: 3,
+      [Stat.Accuracy]: 0.4,
+      [Stat.Critical]: 0.4,
+      [Stat.Evasion]: 0.2,
     }
   },
 
@@ -91,9 +91,9 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 1,
       [Stat.Defense]: 5,
       [Stat.Magic]: 1,
-      [Stat.Special]: 1,
-  
       [Stat.HP]: 30,
+
+      [Stat.Special]: 1,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
@@ -103,24 +103,24 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 2,
       [Stat.Defense]: 4,
       [Stat.Magic]: 0,
-      [Stat.Special]: 2,
-  
       [Stat.HP]: 4,
+
+      [Stat.Special]: 2,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
     },
 
     limitBreakPoints: {
-      [Stat.Attack]: 2,
-      [Stat.Defense]: 6,
-      [Stat.Magic]: 2,
+      [Stat.Attack]: 0,
+      [Stat.Defense]: 0,
+      [Stat.Magic]: 0,
+      [Stat.HP]: 0,
+
       [Stat.Special]: 2,
-  
-      [Stat.HP]: 30,
-      [Stat.Accuracy]: 0,
-      [Stat.Critical]: 0,
-      [Stat.Evasion]: 0,
+      [Stat.Accuracy]: 0.2,
+      [Stat.Critical]: 0.2,
+      [Stat.Evasion]: 0.6,
     }
   },
 
@@ -129,9 +129,9 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 1,
       [Stat.Defense]: 1,
       [Stat.Magic]: 4,
-      [Stat.Special]: 2,
-  
       [Stat.HP]: 15,
+
+      [Stat.Special]: 2,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
@@ -141,24 +141,24 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 0,
       [Stat.Defense]: 1,
       [Stat.Magic]: 2,
-      [Stat.Special]: 2,
-  
       [Stat.HP]: 4,
+
+      [Stat.Special]: 2,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
     },
 
     limitBreakPoints: {
-      [Stat.Attack]: 2,
-      [Stat.Defense]: 2,
-      [Stat.Magic]: 4,
+      [Stat.Attack]: 0,
+      [Stat.Defense]: 0,
+      [Stat.Magic]: 0,
+      [Stat.HP]: 0,
+
       [Stat.Special]: 4,
-  
-      [Stat.HP]: 20,
-      [Stat.Accuracy]: 0,
-      [Stat.Critical]: 0,
-      [Stat.Evasion]: 0,
+      [Stat.Accuracy]: 0.4,
+      [Stat.Critical]: 0.2,
+      [Stat.Evasion]: 0.4,
     }
   },
   
@@ -167,9 +167,9 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 4,
       [Stat.Defense]: 1,
       [Stat.Magic]: 2,
-      [Stat.Special]: 1,
-  
       [Stat.HP]: 20,
+
+      [Stat.Special]: 1,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
@@ -179,24 +179,24 @@ export const StatAllocationMatrix: Record<Archetype, ArchetypeStatMatrix> = {
       [Stat.Attack]: 2,
       [Stat.Defense]: 0,
       [Stat.Magic]: 2,
-      [Stat.Special]: 3,
-  
       [Stat.HP]: 4,
+
+      [Stat.Special]: 3,
       [Stat.Accuracy]: 0,
       [Stat.Critical]: 0,
       [Stat.Evasion]: 0,
     },
 
     limitBreakPoints: {
-      [Stat.Attack]: 4,
+      [Stat.Attack]: 0,
       [Stat.Defense]: 0,
-      [Stat.Magic]: 4,
-      [Stat.Special]: 4,
-  
-      [Stat.HP]: 20,
-      [Stat.Accuracy]: 0,
-      [Stat.Critical]: 0,
-      [Stat.Evasion]: 0,
+      [Stat.Magic]: 0,
+      [Stat.HP]: 0,
+
+      [Stat.Special]: 2,
+      [Stat.Accuracy]: 0.6,
+      [Stat.Critical]: 0.2,
+      [Stat.Evasion]: 0.2,
     }
   },
 }

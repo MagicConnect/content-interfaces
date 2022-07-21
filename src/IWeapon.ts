4d9ab135964c571895d3e0625a3c49701d2aa5f6
Kyle Kemp <kyle@seiyria.com>
2022-07-21 09:58:55 -0500

    refactor item interfaces

--- src/IWeapon.ts
+++ src/IWeapon.ts
@@ -5,31 +5,19 @@ import {
   PrimaryStat,
   primaryStatT,
 } from './BuildingBlocks';
-import { IItem, itemT } from './IItem';
+import { equippableItemT, IEquippableItem, IItem, itemT } from './IItem';
 
-export type IWeapon = IItem & {
-  weaponType: Weapon; // the type of weapon
-  stars: 1 | 2 | 3 | 4 | 5; // weapon rarity
-  primaryStat: PrimaryStat; // weapon main stat
-  secondaryStat?: PrimaryStat; // weapon secondary stat
+export type IWeapon = IItem & IEquippableItem & {
+  weaponType: Weapon;           // the type of weapon
+  secondaryStat?: PrimaryStat;  // weapon secondary stat
   hasHigherPotential?: boolean; // whether the weapon has a higher potential than other items of its type (f.ex. banner weapons have a higher stat potential)
-
-  abilities: string[]; // weapon base abilities (un-upgraded)
 };
 
 export const weaponT: t.Type<IWeapon> = t.intersection([
   itemT,
+  equippableItemT,
   t.type({
     weaponType: weaponTypeT,
-    stars: t.union([
-      t.literal(1),
-      t.literal(2),
-      t.literal(3),
-      t.literal(4),
-      t.literal(5),
-    ]),
-    primaryStat: primaryStatT,
-    abilities: t.array(t.string),
   }),
   t.partial({
     secondaryStat: primaryStatT,
